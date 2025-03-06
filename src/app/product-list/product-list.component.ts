b9057016df6b6332221797ea4da3e716331f31be
AkshayJayachandranVV <akshayjayachandranvv@gmail.com>
2025-03-06 13:26:13 +0530

    first commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+      name: string = "IPhone 12";
+      price: number = 999.99;
+      color: string = "Black Matte";
+      stock:number = 4
+      getDetails() {
+        return this.price * this.price
+      }
+}
