4f2da2bdf73ee80491c3f889474c64f89e58bf20
AkshayJayachandranVV <akshayjayachandranvv@gmail.com>
2025-03-10 13:36:52 +0530

    ngFor

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,16 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-      name: string = "IPhone 12";
-      price: number = 999.99;
-      color: string = "Black Matte";
-      stock:number = 4
-      getDetails() {
-        return this.price * this.price
-      }
+      // name: string = "IPhone 12";
+      // price: number = 999.99;
+      // color: string = "Black Matte";
+      // stock:number = 4
+      // getDetails() {
+      //   return this.price * this.price
+      // }
 
-      onNameChange(){
-        this.name = "Mark"
-      }
+      // onNameChange(event : any){
+      //   this.name = event.target.value
+      // }
+      
 
+      // Increment(){
+      //    this.stock ++
+      // }
+
+      // Decrement(){
+      //   if(this.stock <1) return
+      //  this.stock --
+      // }
 }
